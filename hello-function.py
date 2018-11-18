23c98e7a4997231a3f844557985d849463ffb241
obaramikako <quartersilver@gmail.com>
2018-11-18 09:21:57 +0900

    関数の定義を学んだ

---
+++ hello-function.py
@@ -0,0 +1,27 @@
+# def hello():
+#    print()
+
+# hello()
+
+
+def say_hello(name):
+    print(f"HI {name}")
+
+
+say_hello("Bob")
+
+
+def doble(number):
+    return 2 * number
+
+
+result_1 = doble(3)
+
+print(result_1)
+
+
+def str_combine(str1, str2):
+    return f"{str1,str2"
+
+
+str_cmnbine("Kazuma", "Takahashi")
