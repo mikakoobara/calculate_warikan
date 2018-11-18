16b6eb0f1f3b770aef378ea904681a0f4f0fbccc
obaramikako <quartersilver@gmail.com>
2018-11-18 09:43:54 +0900

    割り勘　３テーブル分

--- warikan.py
+++ warikan.py
@@ -9,3 +9,15 @@ amount = 1500
 number_of_people = 3
 
 print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+# Table2:2000# 円を３人で
+amount = 2000
+number_of_people = 3
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+# Table3:3647円を4人で
+amount = 3647
+number_of_people = 4
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
