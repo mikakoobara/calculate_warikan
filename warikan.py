2a3eae5e00aadafc5a447e00ee3a638b58564415
obaramikako <quartersilver@gmail.com>
2018-11-18 09:41:56 +0900

    割り勘　３テーブルの途中経過

---
+++ warikan.py
@@ -0,0 +1,11 @@
+print(1500 / 3)
+print(2000 / 3)
+print(3647 / 4)
+
+# テーブル毎に割り勘
+
+# Table1:1500円を３人で
+amount = 1500
+number_of_people = 3
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
