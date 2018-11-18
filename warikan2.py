8441861ec88058f5fe155bc23ee2824ba8b6e20b
obaramikako <quartersilver@gmail.com>
2018-11-18 10:20:42 +0900

    割り勘リファクタリング
    変更が起きたらどうなるか（追加、削除）

--- warikan2.py
+++ warikan2.py
@@ -5,3 +5,4 @@ def warikan(amount, number_of_people):
 print(warikan(amount=1500, number_of_people=3))
 print(warikan(amount=2000, number_of_people=3))
 print(warikan(amount=3000, number_of_people=4))
+
